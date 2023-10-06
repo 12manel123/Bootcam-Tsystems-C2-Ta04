b437473f2f853058c375d1e9b15e667348ea6ccc
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-06 17:35:40 +0200

    test

---
+++ Ex05/src/AppMain.java
@@ -0,0 +1,8 @@
+
+public class AppMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Hello
+	}
+
+}
---
+++ HelloWorld/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,7 @@
+package helloworldapp;
+
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+	}
+}
